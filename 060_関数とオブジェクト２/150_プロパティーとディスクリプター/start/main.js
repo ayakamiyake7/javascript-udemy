0227a4fdee2b9112d5bf690fce2e7474fa905cdd
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-07-19 13:02:19 +0100

    Function and Object2.

---
+++ 060_関数とオブジェクト２/150_プロパティーとディスクリプター/start/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// const obj = { prop: 0 };
+
+const obj = {};
+
+Object.defineProperty(obj, "prop", { value: 0, writable: true });
+Object.defineProperty(obj, "prop", { enumerable: true });
+
+obj.prop = 1;
+console.log(obj.prop);
+
+const descriptor = Object.getOwnPropertyDescriptor(obj, "prop", {});
+
+console.log(descriptor);
